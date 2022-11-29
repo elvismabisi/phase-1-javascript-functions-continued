20517d0004bf29599ca7b06d214bba29bde1cf48
Gregory <elvismabisi@gmail.com>
2022-11-29 12:36:53 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+
+function saturdayFun(rollerskate) {
+    if (rollerskate) {
+        return "This Saturday, I want to bathe my dog!"
+    }else{
+        return "This Saturday, I want to roller-skate!"
+    }
+}
+
+function mondayWork(goToOffice){
+    if (goToOffice) {
+        return "This Monday, I will work from home."
+    }else {
+        return "This Monday, I will go to the office."
+    }
+}
+
+function wrapAdjective(string = "*" ) {
+    const innerFunction= function(adjective = "special"){
+       return `You are ${string}${adjective}${string}!`
+    }
+    return innerFunction  
+}
